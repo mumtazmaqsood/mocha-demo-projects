eabf36073450eff2d1b92de261452d0e788fc3b0
Mumtaz <mumtaz.maqsood@yahoo.com>
2021-07-14 11:53:44 +0200

    chai asseration added

---
+++ test/chai.spec.js
@@ -0,0 +1,18 @@
+
+const {expect, assert} = require('chai');
+const should = require('chai').should;
+
+describe('Chai Asseration', () => {
+    const a = 20;
+    arr = [10, 20, 30];
+    myFun = (a, b) => a + b;
+    isTrue = true;
+    nan = NaN;
+    myUndefined = undefined;
+})
+
+it('Assert Style', () => {
+    assert.ok(true) 
+    assert.typeOf(arr, 'array')
+    
+});
